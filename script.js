827b1fb86128032b2dda838310311c4c4f74516b
IgorPanasik <102683696+IgorPanasik@users.noreply.github.com>
2025-03-13 18:03:45 +0300

    feat: add the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+// 1. The logic to get the computer choice
+const getComputerChoice = () => {
+	// Array of choices
+	const choices = ['Rock', 'Paper', 'Scissors'];
+	// Random index between 0 and 2
+	const randomIndex = Math.floor(Math.random() * choices.length);
+	// Return the choice at the random index
+	return choices[randomIndex];
+};
