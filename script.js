c88b4e937a29ad4673d439121ab7a203b78f6860
IgorPanasik <102683696+IgorPanasik@users.noreply.github.com>
2025-03-14 00:23:57 +0300

    feat: Write the logic to play a single round
    Change in function getHumanChoice: add selection to array.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// 3. Declare the players score variables
+let humanScore = 0;
+let computerScore = 0;
+
 // 1. The logic to get the computer choice
 const getComputerChoice = () => {
 	// Array of choices
-	const choices = ['Rock', 'Paper', 'Scissors'];
+	const choices = ['rock', 'paper', 'scissors'];
 	// Random index between 0 and 2
 	const randomIndex = Math.floor(Math.random() * choices.length);
 	// Return the choice at the random index
@@ -10,17 +14,50 @@ const getComputerChoice = () => {
 
 // 2. The logic to get the human choice
 const getHumanChoice = () => {
-	const userInput = prompt('Your choice');
+	// Save user input, convert to lowerCase input and remove spaces
+	const userInput = prompt('Your choice').toLocaleLowerCase().trim();
+	const userChoice = ['rock', 'paper', 'scissors'];
+
 	if (!userInput)
-		return alert(
-			'Please make your choice again, the entry should not be empty',
-		);
-	if (
-		userInput.toLowerCase() !== 'rock' &&
-		userInput.toLowerCase() !== 'paper' &&
-		userInput.toLowerCase() !== 'scissors'
-	) {
+		alert('Please make your choice again, the entry should not be empty');
+
+	if (!userChoice.includes(userInput)) {
 		alert('You should introduce rock, scissors or paper');
+		return;
 	}
 	return userInput;
 };
+
+// 4. Write the logic to play a single round
+const playRound = (computerChoice, humanChoice) => {
+	const winConditions = {
+		rock: 'scissors',
+		paper: 'rock',
+		scissors: 'paper',
+	};
+	if (humanChoice === computerChoice) {
+		console.log('Draw!');
+	} else if (winConditions[humanChoice] === computerChoice) {
+		console.log(
+			`You Win! ${
+				humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+			} beats ${
+				computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+			}`,
+		);
+		humanScore += 1;
+	} else {
+		console.log(
+			`Computer Win! ${
+				computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+			} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`,
+		);
+		computerScore += 1;
+	}
+
+	console.log(`Computer: ${computerChoice}, Human: ${humanChoice}`);
+	console.log(`Computer: ${computerScore}, Human: ${humanScore}`);
+};
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+playRound(computerSelection, humanSelection);
