f3f4cf8b7ec5d221e38b213f5f92a0185cdb11be
IgorPanasik <102683696+IgorPanasik@users.noreply.github.com>
2025-03-15 03:23:55 +0300

    refactor: extracted playRound() from playGame() for better modularity
    Refactored the playGame() function by extracting playRound()
    as a separate callable function.
    Now playRound() is invoked independently to improve code
    modularity and readability.

--- script.js
+++ script.js
@@ -1,76 +1,78 @@
-// 1. The logic to get the computer choice
 const getComputerChoice = () => {
-	// Array of choices
 	const choices = ['rock', 'paper', 'scissors'];
-	// Random index between 0 and 2
 	const randomIndex = Math.floor(Math.random() * choices.length);
-	// Return the choice at the random index
+
 	return choices[randomIndex];
 };
 
-// 2. The logic to get the human choice
 const getHumanChoice = () => {
-	// Save user input, convert to lowerCase input and remove spaces
-	const userInput = prompt('Your choice').toLocaleLowerCase().trim();
+	const userInput = prompt('Your choice');
+
+	if (userInput === null) {
+		alert('You canceled the input');
+		return;
+	}
+
+	const trimmedInput = userInput.toLowerCase().trim();
 	const userChoice = ['rock', 'paper', 'scissors'];
 
-	if (!userInput)
+	if (!trimmedInput) {
 		alert('Please make your choice again, the entry should not be empty');
+		return;
+	}
 
 	if (!userChoice.includes(userInput)) {
 		alert('You should introduce rock, scissors or paper');
 		return;
 	}
-	return userInput;
+
+	return trimmedInput;
 };
 
-// 5. The logic to play the entire game
+const playRound = (humanScore, computerScore) => {
+	const computerChoice = getComputerChoice();
+	const humanChoice = getHumanChoice();
+
+	const winConditions = {
+		rock: 'scissors',
+		paper: 'rock',
+		scissors: 'paper',
+	};
+
+	if (humanChoice === computerChoice) {
+		console.log('Draw!');
+		// The key is the user input and the value is the computer's choice
+	} else if (winConditions[humanChoice] === computerChoice) {
+		console.log(
+			`You Win! ${
+				humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+			} beats ${
+				computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+			}`,
+		);
+		humanScore += 1;
+	} else {
+		console.log(
+			`Computer Win! ${
+				computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+			} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`,
+		);
+		computerScore += 1;
+	}
+
+	console.log(`Computer: ${computerChoice}, Human: ${humanChoice}`);
+	console.log(`Computer: ${computerScore}, Human: ${humanScore}`);
+};
 
 const playGame = () => {
-	// 3. Declare the players score variables
 	let humanScore = 0;
 	let computerScore = 0;
 
-	// 4. Write the logic to play a single round
-	const playRound = () => {
-		const computerChoice = getComputerChoice();
-		const humanChoice = getHumanChoice();
-
-		// An object containing winning cases
-		const winConditions = {
-			rock: 'scissors',
-			paper: 'rock',
-			scissors: 'paper',
-		};
-
-		if (humanChoice === computerChoice) {
-			console.log('Draw!');
-		} else if (winConditions[humanChoice] === computerChoice) {
-			// The key is the user input and the value is the computer's choice
-			console.log(
-				`You Win! ${
-					humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
-				} beats ${
-					computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
-				}`,
-			);
-			humanScore += 1;
-		} else {
-			console.log(
-				`Computer Win! ${
-					computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
-				} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`,
-			);
-			computerScore += 1;
-		}
-
-		console.log(`Computer: ${computerChoice}, Human: ${humanChoice}`);
-		console.log(`Computer: ${computerScore}, Human: ${humanScore}`);
-	};
 	let times = 5;
 	while (times > 0) {
-		playRound();
-		times--;
+		// Call playRound 5 times
+		playRound(humanScore, computerScore);
+		times -= 1;
 	}
 
 	console.log(
